32cfcfc5120b36a0dc798c9d0f06b6f762eec9cd
MrKafiri <kafiri.mr@gmail.com>
2022-03-28 17:51:36 +0430

    Create TimeAgo in detailPage

--- lib/pages/show_article.dart
+++ lib/pages/show_article.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tec/models/result.dart';
+import 'package:intl/intl.dart';
 
 class ShowArticle extends StatelessWidget {
   const ShowArticle({Key? key, required this.item}) : super(key: key);
@@ -133,7 +134,7 @@ class ShowArticle extends StatelessWidget {
             ),
             SizedBox(width: 10),
             Text(
-              item.date.toString(),
+              timeAgoSinceDate(numericDates: true, dt: item.date.toString()),
               style: TextStyle(
                 fontFamily: 'MyFontMedium',
                 fontSize: 16,
@@ -144,5 +145,42 @@ class ShowArticle extends StatelessWidget {
         )
       ],
     );
+  }
+
+  String timeAgoSinceDate({bool numericDates = true, required String dt}) {
+    DateTime date = DateFormat("dd-MM-yyyy h:mma").parse(dt);
+
+    print(date);
+    final date2 = DateTime.now().toLocal();
+    final difference = date2.difference(date);
+
+    if (difference.inSeconds < 5) {
+      return 'Just now';
+    } else if (difference.inSeconds <= 60) {
+      return '${difference.inSeconds} seconds ago';
+    } else if (difference.inMinutes <= 1) {
+      return (numericDates) ? '1 minute ago' : 'A minute ago';
+    } else if (difference.inMinutes <= 60) {
+      return '${difference.inMinutes} minutes ago';
+    } else if (difference.inHours <= 1) {
+      return (numericDates) ? '1 hour ago' : 'An hour ago';
+    } else if (difference.inHours <= 60) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inDays <= 1) {
+      return (numericDates) ? '1 day ago' : 'Yesterday';
+    } else if (difference.inDays <= 6) {
+      return '${difference.inDays} days ago';
+    } else if ((difference.inDays / 7).ceil() <= 1) {
+      return (numericDates) ? '1 week ago' : 'Last week';
+    } else if ((difference.inDays / 7).ceil() <= 4) {
+      return '${(difference.inDays / 7).ceil()} weeks ago';
+    } else if ((difference.inDays / 30).ceil() <= 1) {
+      return (numericDates) ? '1 month ago' : 'Last month';
+    } else if ((difference.inDays / 30).ceil() <= 30) {
+      return '${(difference.inDays / 30).ceil()} months ago';
+    } else if ((difference.inDays / 365).ceil() <= 1) {
+      return (numericDates) ? '1 year ago' : 'Last year';
+    }
+    return '${(difference.inDays / 365).floor()} years ago';
   }
 }
